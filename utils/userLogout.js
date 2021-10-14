5d15f124a35c202a3be205378c22916cdaede400
Saksham Chaturvedi <sakshamc2001@gmail.com>
2021-10-15 00:57:00 +0530

    login before logging out edge case

--- utils/userLogout.js
+++ utils/userLogout.js
@@ -1,9 +1,11 @@
 userLogout = async (req, res, next) => {
   try {
+    if (!req.session.user) {
+      res.status(400).send("Please Login first.");
+    }
     await req.session.destroy();
     res.status(200).send("User logout successful.");
   } catch (err) {
-    console.log(err);
     res.status(400).send(err);
   }
 };
