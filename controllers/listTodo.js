bcc9a855ae4f3bea87b5760e9ff37534ab10fa4f
Saksham Chaturvedi <sakshamc2001@gmail.com>
2021-10-15 01:52:17 +0530

    .

--- controllers/listTodo.js
+++ controllers/listTodo.js
@@ -2,20 +2,19 @@ const todo = require("../models/todo");
 
 const listTodo = async (req, res) => {
   try {
-    await todo
-      .findAll({
+    const result;
+    if (
+       result= await todo.findAll({
         where: {
           userId: req.session.user.id,
         },
         order: ["id"],
       })
-      .then((result) => {
-        if (result) {
-          res.status(200).json(result);
-        } else {
-          res.status(400).send("No tasks to display.");
-        }
-      });
+    ) {
+      res.status(200).json(result);
+    } else {
+      res.status(400).send("No tasks to display.");
+    }
   } catch (err) {
     res.status(400).send(err);
   }
